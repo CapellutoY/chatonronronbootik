41c7c8676fbcd830fd62dcdd37acd3fda4207f47
Spliit <mcuville@gmail.com>
2018-11-28 15:05:24 +0100

    coucou

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if user_session
+      unless Cart.find_by(user_id: current_user.id)
+        Cart.new(user_id: current_user.id).save
+      end
+    end
   end
 end
