eb311128782d2a366071523f0f5fe8d8de3e0d9a
JostQ <dremsis64@hotmail.fr>
2018-11-29 19:12:04 +0100

    ajout dans le cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,9 @@ class CartsController < ApplicationController
   def show
     @cart = Cart.find_by(user_id: current_user.id)
   end
+  def update
+    User.find(current_user.id).cart.items << Item.find(params[:id])
+  end
   def destroy
     cart = Cart.find_by(user_id: current_user.id)
     cart.items.delete(Item.find(params[:id]))
