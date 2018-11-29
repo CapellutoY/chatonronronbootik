851f7c87ca5a8021521f108f75e3328f7f1288ab
JostQ <dremsis64@hotmail.fr>
2018-11-29 21:10:26 +0100

    bonjour e*****

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,5 +1,12 @@
 class ChargesController < ApplicationController
   def new
+    Order.new(user_id: current_user.id).save
+    price = 0
+    Cart.find(current_user.id).items.each do |item|
+      User.find(current_user.id).orders.last.items << item
+      price += item.price
+    end
+    User.find(current_user.id).orders.last.update(price: price)
     @amount = User.find(current_user.id).orders.last.price
   end
   def create
@@ -11,13 +18,6 @@ class ChargesController < ApplicationController
     UserMailer.order_buyer(User.find(current_user.id)).deliver
     UserMailer.order_admin(User.find(current_user.id)).deliver
     Cart.find(current_user.id).items.delete(Item.all)
-    Order.new(user_id: current_user.id).save
-    price = 0
-    Cart.find(current_user.id).items.each do |item|
-      User.find(current_user.id).orders.last.items << item
-      price += item.price
-    end
-    User.find(current_user.id).orders.last.update(price: price)
     redirect_to root_path
   rescue Stripe::CardError => e
 
